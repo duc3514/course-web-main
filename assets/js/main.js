db91330f5e691690735efde5170a40cb0d9fdbef
duc3514 <haupro351444444@gmail.com>
2022-07-09 19:18:18 +0700

    Update 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,5 +28,94 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }, 3000)
 
+    // Fake
+    const courses = [
+        {
+            id:1,
+            people: 25,
+            total_hours: '1:20',
+            title: 'Python par la pratique : 101 Exercices Corrigés',
+            img: 'https://wallpaperaccess.com/full/187161.jpg',
+            rate: 4.5,
+            dollars: 99
+        },{
+            id:2,
+            people: 25,
+            total_hours: '1:20',
+            title: 'Python par la pratique : 101 Exercices Corrigés',
+            img: 'https://wallpaperaccess.com/full/187161.jpg',
+            rate: 4.5,
+            dollars: 99
+        },{
+            id:3,
+            people: 30,
+            total_hours: '1:20',
+            title: 'Python par la pratique : 101 Exercices Corrigés',
+            img: 'https://wallpaperaccess.com/full/187161.jpg',
+            rate: 4.5,
+            dollars: 99
+        },{
+            id:4,
+            people: 120,
+            total_hours: '1:20',
+            title: 'Python par la pratique : 101 Exercices Corrigés',
+            img: 'https://wallpaperaccess.com/full/187161.jpg',
+            rate: 4.5,
+            dollars: 99
+        },{
+            id:5,
+            people: 155,
+            total_hours: '1:20',
+            title: 'Python par la pratique : 101 Exercices Corrigés',
+            img: 'https://wallpaperaccess.com/full/187161.jpg',
+            rate: 4.5,
+            dollars: 99
+        },
+        {
+            id:6,
+            people: 200,
+            total_hours: '1:20',
+            title: 'Python par la pratique : 101 Exercices Corrigés',
+            img: 'https://wallpaperaccess.com/full/187161.jpg',
+            rate: 4.5,
+            dollars: 99
+        },
+    ]
+
+    const cards = document.querySelector('.cards')
+    let htmls = ''
+    courses.map((course) => {
+        htmls += `
+            <div class="l-4">
+            <div class="card">
+                <img src="${course.img}">
+                <div class="card-inner">
+                    <div class="card-info">
+                        <span>
+                            <i class="fa-solid fa-user"></i>
+                            ${course.people}
+                        </span>
+                        <span>
+                            <i class="fa-solid fa-clock"></i>
+                            ${course.total_hours}
+                        </span>
+                    </div>
+                    <h3 class="card-title">${course.title}</h3>
+                    <div class="card-info">
+                        <span class="card-rate">
+                            <i class="fa-solid fa-star"></i>
+                            ${course.rate}
+                            (250)
+                        </span>
+                        <span class="card-price">99</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    })
+
+    cards.innerHTML = htmls
+    
 
 })
